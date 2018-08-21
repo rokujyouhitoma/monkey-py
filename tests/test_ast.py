041807dba404998ed50ee9d606eaab58486845c2
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2018-08-21 19:41:19 +0900

    Add tests/test_ast.py

---
+++ tests/test_ast.py
@@ -0,0 +1,19 @@
+import unittest
+
+from monkey import _token as token
+from monkey.ast import Identifier, LetStatement, Program
+
+
+class TestAst(unittest.TestCase):
+    def test_string(self):
+        program = Program(Statements=[
+            LetStatement(
+                Token=token.Token(Type=token.LET, Literal='let'),
+                Name=Identifier(
+                    Token=token.Token(Type=token.IDENT, Literal='myVar'), Value='myVar'),
+                Value=Identifier(
+                    Token=token.Token(Type=token.IDENT, Literal='anotherVar'), Value='anotherVar'))
+        ])
+
+        if program.String() != 'let myVar = anotherVar;':
+            self.fail('program.String() wrong. got="%s"' % program.String())
